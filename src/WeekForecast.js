8be51bf4088b4fa3211d40049bd593c4ab1c3c56
gjaic2016 <gjaic23@gmail.com>
2023-04-18 00:45:30 +0200

    minor fixes

--- src/WeekForecast.js
+++ src/WeekForecast.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
+import { FaArrowLeft } from "react-icons/fa";
 
 const WeekForecast = () => {
   let history = useHistory();
@@ -10,9 +11,9 @@ const WeekForecast = () => {
 
   return (
     <>
-      <button onClick={goBack}>Back</button>
-
+      <FaArrowLeft className="goBackButton" onClick={goBack}></FaArrowLeft>
       <main className="week_section">
+       
         <div className="week_section_size section_week">
           <div className="icon">
             <h3>DAY</h3>
@@ -24,7 +25,22 @@ const WeekForecast = () => {
             <h3>Few Clouds</h3>
           </div>
           <div className="temperature">
-            <h1>33°C</h1>
+            <h2>33°C</h2>
+          </div>
+        </div>
+        
+        <div className="week_section_size section_week">
+          <div className="icon">
+            <h3>DAY</h3>
+            <h3>London, GB</h3>
+            <img
+              src="http://openweathermap.org/img/wn/02d@2x.png"
+              alt="weatherIcon"
+            />
+            <h3>Few Clouds</h3>
+          </div>
+          <div className="temperature">
+            <h2>33°C</h2>
           </div>
         </div>
         <div className="week_section_size section_week">
@@ -38,7 +54,7 @@ const WeekForecast = () => {
             <h3>Few Clouds</h3>
           </div>
           <div className="temperature">
-            <h1>33°C</h1>
+            <h2>33°C</h2>
           </div>
         </div>
         <div className="week_section_size section_week">
@@ -52,7 +68,7 @@ const WeekForecast = () => {
             <h3>Few Clouds</h3>
           </div>
           <div className="temperature">
-            <h1>33°C</h1>
+            <h2>33°C</h2>
           </div>
         </div>
         <div className="week_section_size section_week">
@@ -66,21 +82,7 @@ const WeekForecast = () => {
             <h3>Few Clouds</h3>
           </div>
           <div className="temperature">
-            <h1>33°C</h1>
-          </div>
-        </div>
-        <div className="week_section_size section_week">
-          <div className="icon">
-            <h3>DAY</h3>
-            <h3>London, GB</h3>
-            <img
-              src="http://openweathermap.org/img/wn/02d@2x.png"
-              alt="weatherIcon"
-            />
-            <h3>Few Clouds</h3>
-          </div>
-          <div className="temperature">
-            <h1>33°C</h1>
+            <h2>33°C</h2>
           </div>
         </div>
       </main>
